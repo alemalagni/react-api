cb7259ca52a636aa26a374c68f41b205b1115b36
alemalagni <alemalagni@gmail.com>
2025-04-15 15:39:57 +0200

    feat(app): fix lista

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,21 @@ import './App.css'
 
 function App() {
   function fetchLista() {
+    const lista = [];
     axios.get('https://www.freetestapi.com/api/v1/actors')
       .then(data => {
-        const lista = data;
-        console.log(lista.data);
+        for (let i = 0; i < data.data.length; i++) {
+          lista.push({
+            immagine: data.data[i].image,
+            nome: data.data[i].name,
+            annoNascita: data.data[i].birth_year,
+            nazionalità: data.data[i].nationality,
+            riconoscimenti: data.data[i].awards,
+            biografia: data.data[i].biography
+          });
+        }
+
+        console.log(lista);
       })
   }
 
