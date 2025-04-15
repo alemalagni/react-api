07b3a1edf00812240d64e1a00bb02c4595e98a98
alemalagni <alemalagni@gmail.com>
2025-04-15 15:14:07 +0200

    feat(app): add fetch function

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import { useState } from 'react'
+import axios from 'axios';
 import './App.css'
 
 function App() {
+  function fetchLista() {
+    const lista = [];
+    axios.get('https://freetestapi.com/api/v1/actors', (req, res) => {
+      lista = data;
+      console.log(lista);
+    })
+  }
 
+  return (
+    fetchLista()
+  );
 }
 
 export default App
