ab7159e78def04910acb20b2d6deeca848c8f881
alemalagni <alemalagni@gmail.com>
2025-04-15 17:01:18 +0200

    feat(actor): add classname

--- src/components/actor.jsx
+++ src/components/actor.jsx
@@ -2,23 +2,23 @@ import style from './Actor.module.css'
 
 function Actor({ immagine, nome, annoNascita, nazionalità, riconoscimenti, biografia }) {
     return (
-        <div id='actor'>
-            <div id='immagine'>
+        <div className={style.actor}>
+            <div className={style.immagine}>
                 <img src={immagine} alt="" />
             </div>
-            <div id='nome'>
+            <div className={style.nome}>
                 <span>{nome}</span>
             </div>
-            <div id='annoNascita'>
+            <div className={style.annoNascita}>
                 <span>{annoNascita}</span>
             </div>
-            <div id='nazionalità'>
+            <div className={style.nazionalità}>
                 <span>{nazionalità}</span>
             </div>
-            <div id='riconoscimenti'>
+            <div className={style.riconoscimenti}>
                 <span>{riconoscimenti}</span>
             </div>
-            <div id='biografia'>
+            <div className={style.biografia}>
                 <span>{biografia}</span>
             </div>
         </div>
