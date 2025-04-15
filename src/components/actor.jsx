a9aed571bdaf70bc0e6e189825b68a27f1a6f0df
alemalagni <alemalagni@gmail.com>
2025-04-15 16:51:38 +0200

    feat(actor): add id

--- src/components/actor.jsx
+++ src/components/actor.jsx
@@ -2,23 +2,23 @@ import style from './Actor.module.css'
 
 function Actor({ immagine, nome, annoNascita, nazionalità, riconoscimenti, biografia }) {
     return (
-        <div>
-            <div>
+        <div id='actor'>
+            <div id='immagine'>
                 <img src={immagine} alt="" />
             </div>
-            <div>
+            <div id='nome'>
                 <span>{nome}</span>
             </div>
-            <div>
+            <div id='annoNascita'>
                 <span>{annoNascita}</span>
             </div>
-            <div>
+            <div id='nazionalità'>
                 <span>{nazionalità}</span>
             </div>
-            <div>
+            <div id='riconoscimenti'>
                 <span>{riconoscimenti}</span>
             </div>
-            <div>
+            <div id='biografia'>
                 <span>{biografia}</span>
             </div>
         </div>
