7c1604d973fc11287483a7e4ab6e03c337f8c157
alemalagni <alemalagni@gmail.com>
2025-04-15 15:19:09 +0200

    fix(app):

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import './App.css'
 
 function App() {
   function fetchLista() {
-    axios.get('https://freetestapi.com/api/v1/actors', (req, res) => {
-      const lista = data;
-      console.log(lista);
-    })
+    axios.get('https://freetestapi.com/api/v1/actors')
+      .then(response => response.json())
+      .then(data => {
+        const lista = data;
+        console.log(lista);
+      })
   }
 
   return (
